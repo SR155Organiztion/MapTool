6ee17b506a03603b8d372a464b2e5e5e96e62702
na4742 <126836684+na4742@users.noreply.github.com>
2025-07-13 13:20:32 +0900

    [Style] : 불필요한 코드 제거

--- Client/Code/CLoading.cpp
+++ Client/Code/CLoading.cpp
@@ -74,10 +74,6 @@ _uint CLoading::Loaing_ForStage()
 	(L"Proto_HexPrismTex", Engine::CHexPrismTex::Create(m_pGraphicDev))))
 		return E_FAIL;
 
-	//if (FAILED(CProtoMgr::GetInstance()->Ready_Prototype
-	//(L"Proto_RcTex", Engine::CRcTex::Create(m_pGraphicDev))))
-	//	return E_FAIL;
-
 	lstrcpy(m_szLoading, L"Texture Component Loading...........................");
 
 	if (FAILED(CProtoMgr::GetInstance()->Ready_Prototype
